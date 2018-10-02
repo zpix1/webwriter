886449d69b08d57cab22de877a9efb849bbd1376
Ivan Baksheev <zpix-dev@list.ru>
2018-10-02 22:45:32 +0700

    Design changes

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,7 +3,7 @@
         <h3>Sign In</h3>
         <input v-model="email" type="text" placeholder="email"><br>
         <input v-model="password" type="password" placeholder="password"><br>
-        <button @click="signIn">Log in</button>
+        <button @click="signIn">Log In</button>
         <p class="error">{{ error }}</p>
         <p class="success">{{ success }}</p>
         <p>No account? Create it <router-link to="/sign-up">here</router-link>!</p>
@@ -33,7 +33,8 @@ export default {
         },
         (err) => {
           this.success = ''
-          this.error = err.message
+          this.error = err.code === 'auth/user-not-found' ? 'User not found. Did you enter email and password correctly?' : err.message
+          console.log(err)
         }
       )
     }
